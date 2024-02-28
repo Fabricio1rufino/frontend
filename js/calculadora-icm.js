f62f52cfe7c6d22e4e8f5a06073edb4589a168b8
Fabricio1rufino <fabricioluvizeto930@gmail.com>
2024-02-28 11:55:56 -0300

    calculadora

--- js/calculadora-icm.js
+++ js/calculadora-icm.js
@@ -9,12 +9,28 @@ formulario.addEventListener('submit', function(e){
     const peso = Number(inputpeso.value)
     const altura = Number(inputAltura.value)
 
-    const imc = calcularImc(peso, altura)
-
-    console.log(imc)
+    const imc = calcularImc(peso, altura);
+    const classficacao = tabelaImc(imc)  
+    mostrarResultado(classficacao)
 })
 
 function calcularImc(peso, altura){
     let imc = (peso / altura ** 2);
     return imc.toFixed(2);
+}
+
+function mostrarResultado(msg){
+    const resultado = document.querySelector("#resultado");
+    resultado.innerHTML = msg;
+}
+
+
+function tabelaImc(imc){
+    const classficacao = ['abaixo do peso', 'peso normal', 'sobrepeso','obesidade grau 1','obesidade grau 2','obesidade grau 3']
+    if(imc >= 39.9) return classficacao[5];
+    if(imc >= 34.9) return classficacao[4];
+    if(imc >= 29.9) return classficacao[3];
+    if(imc >= 24.9) return classficacao[2];
+    if(imc >= 18.5) return classficacao[1];
+    if(imc < 18.5) return classficacao[0];
 }
